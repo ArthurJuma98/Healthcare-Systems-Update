139bff8f2205d8d2a16488c0f7e3992c81d4eb24
Arthur Ochilo <arthurjuma98@gmail.com>
2025-05-28 11:01:11 +0300

    user router added

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from . import models
 from .database import engine
-from .router import patient
+from .router import patient, user
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
 app.include_router(patient.router)
+app.include_router(user.router)
