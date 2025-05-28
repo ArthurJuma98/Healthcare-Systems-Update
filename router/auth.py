f325be3601f8017a9840261a42224aecf6c2dcb8
Arthur Ochilo <arthurjuma98@gmail.com>
2025-05-28 15:10:29 +0300

    user authentication setup

---
+++ router/auth.py
@@ -0,0 +1,27 @@
+from fastapi import Depends, HTTPException, status, APIRouter
+from .. import models, database
+from ..hashing import Hash
+from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
+
+router = APIRouter(
+    tags=["Authentication"]
+)
+
+get_db = database.get_db
+
+@router.post("/login")
+def login(request: OAuth2PasswordRequestForm=Depends(), db:Session=Depends(get_db)):
+    user = db.query(models.User).filter(models.User.name == request.username).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Invalid credentials."
+        )
+    
+    if not Hash.verify(request.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Incorrect password"
+        )
