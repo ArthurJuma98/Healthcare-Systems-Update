966f3a15ecb11bbf277ac33e2cdad071a856303a
Arthur Ochilo <arthurjuma98@gmail.com>
2025-06-03 00:19:36 +0300

    auth route registration

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from . import models
 from .database import engine
-from .router import patient, user
+from .router import patient, user, auth
 
 app = FastAPI()
 
@@ -9,3 +9,4 @@ models.Base.metadata.create_all(engine)
 
 app.include_router(patient.router)
 app.include_router(user.router)
+app.include_router(auth.router)
